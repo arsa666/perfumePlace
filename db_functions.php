dd118a1670c30dff3468757ff94f4e063c2d1b64
Arsalan Ahmed <arsa666.k@gmail.com>
2014-03-04 02:52:21 -0500

    Update db_functions.php

--- db_functions.php
+++ db_functions.php
@@ -6,7 +6,7 @@
   {
     
     // Create connection
-    $con=mysqli_connect("localhost","root","666arsa666","arsa666_perfumePlace");
+    $con=mysqli_connect("localhost","user","passs","database");
 
     // Check connection
     if(mysqli_connect_errno())
